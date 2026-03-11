cae7ce6ce04c1848cb8bda3dabd8db744e0f7417
081282996533 <o81282996533@gmail.com>
2026-03-11 13:57:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const s1 = "QUl6YVN5QlR0UXp3c0hSZ0dXdDd6Y0RBQlhfRjhuRXJxdHNSV01V"; // API KEY
+export const MHP_CONFIG = {
+    apiKey: atob(s1),
+    projectId: "portrait-lab-428308",
+    appId: "1:103625933953:android:4e929a8e574ab76308b980"
+};
